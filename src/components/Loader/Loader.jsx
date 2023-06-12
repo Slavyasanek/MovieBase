291b36e7feffb5a252ec5ca2a32c6bcf1cc2aa86
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-06-12 21:11:07 +0300

    create loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import { InfinitySpin } from "react-loader-spinner"
+
+export const Loader = () => {
+    return (
+        <>
+            <InfinitySpin
+                width='200'
+                color="#4fa94d"
+            />
+        </>
+    )
+}
