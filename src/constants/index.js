52200a39965c1bff6192590b8af328692ff437b1
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-06-12 20:23:09 +0300

    add status

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,3 @@
 export * from './theme';
 export * from './GlobalStyle.styled'
+export * from './status';
---
+++ src/constants/status.js
@@ -0,0 +1,6 @@
+export const STATUS = {
+    IDLE: 'idle',
+    PENDING: 'pending',
+    REJECTED: 'rejected',
+    RESOLVED: 'resolved'
+}
