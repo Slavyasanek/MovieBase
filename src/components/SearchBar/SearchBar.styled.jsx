1d59c95750a1031e7909a28e332473d897286176
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-06-13 14:06:32 +0300

    styling serachbar

--- src/components/SearchBar/SearchBar.styled.jsx
+++ src/components/SearchBar/SearchBar.styled.jsx
@@ -1,3 +1,4 @@
+import { devices } from "constants";
 import styled from "styled-components";
 
 export const Form = styled.form`
@@ -14,7 +15,13 @@ export const Search = styled.input`
     background-color: transparent;
     border: 2px solid ${props => props.theme.red};
     outline: none;
+    font-size: 20px;
     font-family: 'Anonymous Pro', monospace;
+    @media screen and (${devices.tablet}) {
+        width: 300px;
+        padding-top: 15px;
+        padding-bottom: 15px;
+    }
 `
 
 export const SubmitBtn = styled.button`
@@ -25,4 +32,23 @@ export const SubmitBtn = styled.button`
     gap: 8px;
     border-radius: 8px;
     font-family: 'Prompt', sans-serif;
+    background-color: ${props => props.theme.darkRed};
+    border: none;
+    padding: 8px;
+    font-size: 16px;
+    cursor: pointer;
+    color: ${props => props.theme.beige};
+    transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+    & svg {
+        width: 30px;
+        height: 30px;
+    }
+    @media screen and (${devices.tablet}) {
+        padding-left: 15px;
+        padding-right: 15px;
+    }
+    &:hover, &:focus {
+        background-color: ${props => props.theme.beige};
+        color: ${props => props.theme.darkRed};
+    }
 `
