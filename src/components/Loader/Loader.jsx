70ed405c64d65008fa8c15bfaba11d7ff60cabb7
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-06-13 15:48:29 +0300

    styling loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,12 +1,13 @@
 import { InfinitySpin } from "react-loader-spinner"
+import { LoaderWrapper } from "./Loader.styled"
 
 export const Loader = () => {
     return (
-        <>
+        <LoaderWrapper>
             <InfinitySpin
                 width='200'
-                color="#4fa94d"
+                color="#440700"
             />
-        </>
+        </LoaderWrapper>
     )
 }
