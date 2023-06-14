bf953ea87a6ccbf68475f6b1955168bc67f51123
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-06-14 14:00:54 +0300

    add lazy load

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
-import { Home } from "pages/Home";
+import { lazy } from "react";
 import { SharedLayout } from "./SharedLayout/SharedLayout";
 import { Route, Routes} from "react-router-dom";
-import { MovieDetails } from "pages/MovieDetails";
-import { Movies } from "pages/Movies";
-import { Cast } from "./Cast/Cast";
-import { Reviews } from "./Reviews/Reviews";
+
+const Home = lazy(() => import('../pages/Home'));
+const MovieDetails = lazy(() => import("../pages/MovieDetails"));
+const Movies = lazy(() => import("../pages/Movies"));
+const Cast = lazy(() => import("./Cast/Cast"));
+const Reviews = lazy(() => import('./Reviews/Reviews'))
 
 export const App = () => {
   return (
@@ -17,6 +19,7 @@ export const App = () => {
             <Route path="cast" element={<Cast/>}/>
             <Route path="reviews" element={<Reviews/>}/>
           </Route>
+          <Route path="*" element={<Home/>}/>
         </Route>
       </Routes>
     </>
