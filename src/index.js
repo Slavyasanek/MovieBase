4490b1c92d3f30dd65746ed36e96cd5dabd57706
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-07-04 12:58:08 +0300

    init

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { theme, GlobalStyle } from 'constants';
 
+console.log(process.env.PUBLIC_URL);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename={process.env.PUBLIC_URL}>
